e4439d580fef3166a617a882167c9ae38ccf38fa
Leonardo Lombardi <leonardolombardi@leonardos-mac.local>
2016-08-02 08:36:16 -0300

    middle of lecture 5

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -9,8 +9,39 @@
 import UIKit
 
 class FaceViewController: UIViewController {
-    var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile)
-
-
+    var expression = FacialExpression(eyes: .Open, eyeBrows: .Relaxed, mouth: .Smile){
+        didSet {
+            updateUI()
+        }
+    }
+    @IBOutlet weak var faceView: FaceView!{
+        didSet {
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(FaceView.changeScale(_:))))
+            updateUI()
+        }
+    }
+    
+    private var mouthCurvatures = [
+        FacialExpression.Mouth.Frown:-1.0,
+        .Grin:0.5,
+        .Smile:1.0,
+        .Neutral:0.0,
+        .Smirk: -0.5]
+    
+    private var eyeBrownTilts = [FacialExpression.EyeBrows.Furrowed:-0.5, .Normal:0.0 , .Relaxed: 0.5]
+    
+    private func updateUI(){
+        switch expression.eyes {
+        case .Open:
+            faceView.eyesOpen = true
+        case .Closed:
+            faceView.eyesOpen = false
+        case .Squinting:
+            faceView.eyesOpen = false
+        }
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        faceView.eyeBrowTilt = eyeBrownTilts[expression.eyeBrows] ?? 0.0
+    }
+    
 }
 
